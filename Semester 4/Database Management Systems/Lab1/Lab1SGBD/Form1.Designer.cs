9d3c304c7678eec6f48be3715877788cfdcee218
916-Racz-Alexandra <alexandra.racz@stud.ubbcluj.ro>
2025-04-09 18:35:24 +0300

    tema 1 submitted

---
+++ Semester 4/Database Management Systems/Lab1/Lab1SGBD/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Lab1SGBD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button3 = new System.Windows.Forms.Button();
+            this.allSuppliers = new System.Windows.Forms.DataGridView();
+            this.animalsGrid = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.allSuppliers)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.animalsGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(225, 428);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(130, 44);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Check connection\r\n\r\n";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // allSuppliers
+            // 
+            this.allSuppliers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.allSuppliers.Location = new System.Drawing.Point(40, 82);
+            this.allSuppliers.Name = "allSuppliers";
+            this.allSuppliers.RowHeadersWidth = 51;
+            this.allSuppliers.RowTemplate.Height = 24;
+            this.allSuppliers.Size = new System.Drawing.Size(453, 308);
+            this.allSuppliers.TabIndex = 1;
+            this.allSuppliers.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView3_CellContentClick);
+            // 
+            // animalsGrid
+            // 
+            this.animalsGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.animalsGrid.Location = new System.Drawing.Point(674, 129);
+            this.animalsGrid.Name = "animalsGrid";
+            this.animalsGrid.RowHeadersWidth = 51;
+            this.animalsGrid.RowTemplate.Height = 24;
+            this.animalsGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.animalsGrid.Size = new System.Drawing.Size(429, 244);
+            this.animalsGrid.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(778, 416);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 40);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Add/Update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(975, 416);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 40);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Delete";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1257, 554);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.animalsGrid);
+            this.Controls.Add(this.allSuppliers);
+            this.Controls.Add(this.button3);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.allSuppliers)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.animalsGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView allSuppliers;
+        private System.Windows.Forms.DataGridView animalsGrid;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Semester 4/Database Management Systems/Lab1/Lab1SGBD/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1SGBD
+{
+    public partial class Form1 : Form
+    {
+
+        SqlDataAdapter daSuppliers;
+        SqlDataAdapter daAnimals;
+        SqlConnection connection;
+
+        DataSet dataSet;
+        BindingSource bsSuppliers;
+        BindingSource bsAnimals;
+
+        string connectionString = @"Data Source=DELL\SQLEXPRESS;Initial Catalog = PetShopDB; Integrated Security = True; TrustServerCertificate=True";
+
+        public Form1()
+        {
+            InitializeComponent();
+            FillData();
+            allSuppliers.SelectionChanged += allSuppliers_selectionChanged;
+        }
+
+
+        string getConnectionString()
+        { return connectionString; }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void FillData()
+        {
+
+            using (this.connection = new SqlConnection(getConnectionString()))
+            {
+                this.connection.Open();
+                daSuppliers = new SqlDataAdapter("Select * from Suppliers", this.connection);
+                daAnimals = new SqlDataAdapter("Select * from Animals", this.connection);
+
+                this.dataSet = new DataSet();
+                this.daSuppliers.Fill(dataSet, "Suppliers");
+                this.daAnimals.Fill(dataSet, "Animals");
+
+
+                bsSuppliers = new BindingSource();
+                bsSuppliers.DataSource = dataSet;
+                bsSuppliers.DataMember = "Suppliers";
+                allSuppliers.DataSource = bsSuppliers;
+
+
+                bsAnimals = new BindingSource();
+                bsAnimals.DataSource = dataSet;
+                bsAnimals.DataMember = "Animals";
+                animalsGrid.DataSource = bsAnimals;
+
+                connection.Close();
+
+
+            }
+
+
+
+
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection cn = new SqlConnection(connectionString);
+                if (cn.State == System.Data.ConnectionState.Closed)
+                    cn.Open();
+                MessageBox.Show("Test connection succeded", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dataGridView3_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void allSuppliers_selectionChanged(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(getConnectionString()))  
+            {
+                conn.Open();
+
+                if (allSuppliers.CurrentRow != null)
+                {
+                    int supID = Convert.ToInt32(allSuppliers.CurrentRow.Cells["supplierID"].Value);
+
+                    SqlCommand cmd = new SqlCommand("SELECT * FROM Animals WHERE supplierID = @supplierID", conn);
+                    cmd.Parameters.AddWithValue("@supplierID", supID);
+
+                    DataView dv = new DataView(dataSet.Tables["Animals"]);
+                    dv.RowFilter = $"supplierID = {supID}";
+                    bsAnimals.DataSource = dv;
+                }
+            }
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                bsSuppliers.EndEdit();
+                bsAnimals.EndEdit();
+                allSuppliers.EndEdit();
+                animalsGrid.EndEdit();
+
+                if (dataSet.HasChanges())
+                {
+                    using (SqlConnection conn = new SqlConnection(getConnectionString()))
+                    {
+                        conn.Open();
+
+                        daSuppliers.SelectCommand = new SqlCommand("SELECT * FROM Suppliers", conn);
+                        daAnimals.SelectCommand = new SqlCommand("SELECT * FROM Animals", conn);
+
+                        SqlCommandBuilder cmdBuilderSuppliers = new SqlCommandBuilder(daSuppliers);
+                        SqlCommandBuilder cmdBuilderAnimals = new SqlCommandBuilder(daAnimals);
+
+                        daSuppliers.Update(dataSet, "Suppliers");
+                        daAnimals.Update(dataSet, "Animals");
+
+                        dataSet.Tables["Animals"].Clear();
+                        daAnimals.Fill(dataSet, "Animals");
+
+                        MessageBox.Show("Updated successfully!");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("No observed updates.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                if (animalsGrid.CurrentRow != null)
+                {
+                    int rowIndex = animalsGrid.CurrentRow.Index;
+                    DialogResult result = MessageBox.Show("Are you sure you want to delete this entry?", "Confirm Deletion", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+                    if (result == DialogResult.Yes)
+                    {
+                        using (SqlConnection conn = new SqlConnection(getConnectionString()))
+                        {
+                            conn.Open();
+
+                            int animalID = Convert.ToInt32(animalsGrid.Rows[rowIndex].Cells["animalID"].Value);
+
+                            DataRow[] animalRow = dataSet.Tables["Animals"].Select($"animalID = {animalID}");
+                            if (animalRow.Length > 0)
+                            {
+                                animalRow[0].Delete();
+                            }
+
+                            daAnimals.DeleteCommand = new SqlCommand("delete from Animals where animalID = @animalID", conn);
+                            daAnimals.DeleteCommand.Parameters.AddWithValue("@animalID", animalID);
+
+
+                            daAnimals.Update(dataSet, "Animals");
+
+                            MessageBox.Show("Deleted successfully!");
+                            animalsGrid.Refresh();
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("No row selected for deletion.");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error while deleting: " + ex.Message);
+            }
+                
+            
+          
+        }
+
+    }
+}
